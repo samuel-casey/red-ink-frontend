5d22e4945c6da87d2866094b870e27dbd01b968d
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-19 14:06:33 -0500

    deleted default files, added react-router

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as firebase from 'firebase';
 
+const firebaseConfig = {
+	apiKey: 'AIzaSyAHI_ndPIp9T-SGIeLkZyCZY4qL9wqwMUQ',
+	authDomain: 'red-ink.firebaseapp.com',
+	databaseURL: 'https://red-ink.firebaseio.com',
+	projectId: 'red-ink',
+	storageBucket: 'red-ink.appspot.com',
+	messagingSenderId: '353136361187',
+	appId: '1:353136361187:web:27b9cc09ecd6e6190f0fd6',
+	measurementId: 'G-CQWPBWG3JJ',
+};
 
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
 	<React.StrictMode>
