e2956952369ba605ef5b0fee202f825d54e6d58e
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-22 16:14:42 -0500

    m

--- src/Components/SubmissionChecklist/SubmissionChecklist.js
+++ src/Components/SubmissionChecklist/SubmissionChecklist.js
@@ -3,32 +3,80 @@ import './SubmissionChecklist.scss';
 
 const SubmissionChecklist = () => {
 	return (
-		<div className='submission-checklist-container'>
-			<div className='instructions-img-container'>
-				<img
-					src='https://res.cloudinary.com/scimgcloud/image/upload/v1606077424/red-ink/gDocsChangeSettings_o1gw8u.png'
-					alt='change-settings-img'
-				/>
+		<>
+			<p id='instructions-intro'>Instructions intro</p>
+			<div className='submission-checklist-container'>
+				<div className='instructions-text'>
+					<span>
+						<div className='checkbox incomplete'>
+							<i className='fas fa-square'></i>
+						</div>
+					</span>
+					<span>
+						Click the "Share" button in the upper righthand corner of your
+						document.
+					</span>
+				</div>
+				<div className='instructions-img-container'>
+					<img
+						src='https://res.cloudinary.com/scimgcloud/image/upload/v1606077424/red-ink/gDocsChangeSettings_o1gw8u.png'
+						alt='change-settings-img'
+					/>
+				</div>
+				<div className='instructions-text'>
+					<span>
+						<div className='checkbox incomplete'>
+							<i className='fas fa-square'></i>
+						</div>
+					</span>
+					<span>
+						Click the "Share" button in the upper righthand corner of your
+						document.
+					</span>
+				</div>
+				<div className='instructions-img-container'>
+					<img
+						src='https://res.cloudinary.com/scimgcloud/image/upload/v1606077556/red-ink/gDocsChangeSettings_ww6azr.png'
+						alt='change-img'
+					/>
+				</div>
+				<div className='instructions-text'>
+					<span>
+						<div className='checkbox incomplete'>
+							<i className='fas fa-square'></i>
+						</div>
+					</span>
+					<span>
+						Click the "Share" button in the upper righthand corner of your
+						document.
+					</span>
+				</div>
+				<div className='instructions-img-container'>
+					<img
+						src='https://res.cloudinary.com/scimgcloud/image/upload/v1606077424/red-ink/gDocsCommenterDropdown_dd42aw.png'
+						alt='commenter-dropdown-img'
+					/>
+				</div>
+				<div className='instructions-text'>
+					<span>
+						<div className='checkbox incomplete'>
+							<i className='fas fa-square'></i>
+						</div>
+					</span>
+					<span>
+						Click the "Share" button in the upper righthand corner of your
+						document.
+					</span>
+				</div>
+				<div className='instructions-img-container'>
+					<img
+						src='https://res.cloudinary.com/scimgcloud/image/upload/v1606077466/red-ink/gDocsGetShareLink_seuqz7.png'
+						alt='copy-link-img'
+					/>
+				</div>
+				<i class='fas fa-check-square'></i>
 			</div>
-			<div className='instructions-img-container'>
-				<img
-					src='https://res.cloudinary.com/scimgcloud/image/upload/v1606077556/red-ink/gDocsChangeSettings_ww6azr.png'
-					alt='change-img'
-				/>
-			</div>
-			<div className='instructions-img-container'>
-				<img
-					src='https://res.cloudinary.com/scimgcloud/image/upload/v1606077424/red-ink/gDocsCommenterDropdown_dd42aw.png'
-					alt='commenter-dropdown-img'
-				/>
-			</div>
-			<div className='instructions-img-container'>
-				<img
-					src='https://res.cloudinary.com/scimgcloud/image/upload/v1606077466/red-ink/gDocsGetShareLink_seuqz7.png'
-					alt='copy-link-img'
-				/>
-			</div>
-		</div>
+		</>
 	);
 };
 
