4c156d9e483faa7302805826d6cd3c8030d7e07d
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-22 22:03:35 -0500

    SubmissionsChecklist complete

--- src/Components/SubmissionChecklist/SubmissionChecklist.js
+++ src/Components/SubmissionChecklist/SubmissionChecklist.js
@@ -9,7 +9,7 @@ const SubmissionChecklist = () => {
 			number: 1,
 			checked: false,
 			text:
-				'1) Click the "Share" button in the upper righthand corner of your document.',
+				'Click the "Share" button in the upper righthand corner of your document.',
 			imgUrl:
 				'https://res.cloudinary.com/scimgcloud/image/upload/v1606077424/red-ink/gDocsChangeSettings_o1gw8u.png',
 			classNameEnabled: true,
@@ -18,7 +18,7 @@ const SubmissionChecklist = () => {
 		{
 			number: 2,
 			checked: false,
-			text: '2) Select "Change" underneath the "Get link" icon.',
+			text: 'Select "Change" underneath the "Get link" icon.',
 			imgUrl:
 				'https://res.cloudinary.com/scimgcloud/image/upload/v1606077556/red-ink/gDocsChangeSettings_ww6azr.png',
 			classNameEnabled: true,
@@ -28,7 +28,7 @@ const SubmissionChecklist = () => {
 			number: 3,
 			checked: false,
 			text:
-				'3) Click the arrow and choose "Commenter" from the list so that your editor can leave comments and suggestions on your document.',
+				'Click the arrow and choose "Commenter" from the list so that your editor can leave comments and suggestions on your document.',
 			imgUrl:
 				'https://res.cloudinary.com/scimgcloud/image/upload/v1606077424/red-ink/gDocsCommenterDropdown_dd42aw.png',
 			classNameEnabled: true,
@@ -38,7 +38,7 @@ const SubmissionChecklist = () => {
 			number: 4,
 			checked: false,
 			text:
-				'4) Click "Done" below the dropdown list, and click "Copy link" to the right of the green box to copy your shareable link to the clipboard. Return to red ink to share your link with your editor!',
+				'Click "Done" below the dropdown list, and click "Copy link" to the right of the green box to copy your shareable link to the clipboard. Return to red ink to share your link with your editor!',
 			imgUrl:
 				'https://res.cloudinary.com/scimgcloud/image/upload/v1606077466/red-ink/gDocsGetShareLink_seuqz7.png',
 			classNameEnabled: true,
@@ -77,6 +77,7 @@ const SubmissionChecklist = () => {
 	const instructions = checked.map((item) => (
 		<div className='instructions-container'>
 			<div className='instructions-text' key={item.number}>
+				<p>{item.number}.</p>
 				<div
 					box-number={item.number.toString()}
 					onClick={() => handleClick(item.number - 1)}
@@ -100,7 +101,13 @@ const SubmissionChecklist = () => {
 
 	return (
 		<div className='submissions-checklist-page'>
-			<p id='instructions-intro'>Instructions intro</p>
+			<p id='instructions-intro'>
+				<span id='red-ink-instructions'>red ink</span> currently utilizes Google
+				Docs as an editing tool. In order to submit your writing to your editor,
+				please take a moment to create a Google Doc, and check the 4 boxes below
+				to enable sharing so that your editor can leave comments and suggestions
+				on your writing!
+			</p>
 			<div className='submission-checklist-container'>{instructions}</div>
 			{proceedButton}
 		</div>
