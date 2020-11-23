5c0dee7dd01a78ad08c5beeaed0b5f603ed0dfc1
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-22 21:47:29 -0500

    SubmissionsChecklist boxes complete

--- src/Components/SubmissionChecklist/SubmissionChecklist.js
+++ src/Components/SubmissionChecklist/SubmissionChecklist.js
@@ -9,32 +9,40 @@ const SubmissionChecklist = () => {
 			number: 1,
 			checked: false,
 			text:
-				'Click the "Share" button in the upper righthand corner of your document.',
+				'1) Click the "Share" button in the upper righthand corner of your document.',
 			imgUrl:
 				'https://res.cloudinary.com/scimgcloud/image/upload/v1606077424/red-ink/gDocsChangeSettings_o1gw8u.png',
+			classNameEnabled: true,
+			hrClass: true,
 		},
 		{
 			number: 2,
 			checked: false,
-			text: 'Select "Change" underneath the "Get link" icon.',
+			text: '2) Select "Change" underneath the "Get link" icon.',
 			imgUrl:
 				'https://res.cloudinary.com/scimgcloud/image/upload/v1606077556/red-ink/gDocsChangeSettings_ww6azr.png',
+			classNameEnabled: true,
+			hrClass: true,
 		},
 		{
 			number: 3,
 			checked: false,
 			text:
-				'Click the arrow and choose "Commenter" from the list so that your editor can leave comments and suggestions on your document.',
+				'3) Click the arrow and choose "Commenter" from the list so that your editor can leave comments and suggestions on your document.',
 			imgUrl:
 				'https://res.cloudinary.com/scimgcloud/image/upload/v1606077424/red-ink/gDocsCommenterDropdown_dd42aw.png',
+			classNameEnabled: true,
+			hrClass: true,
 		},
 		{
 			number: 4,
 			checked: false,
 			text:
-				'Click "Done" below the dropdown list, and click "Copy link" to the right of the green box to copy your shareable link to the clipboard. Return to red ink to share your link with your editor!',
+				'4) Click "Done" below the dropdown list, and click "Copy link" to the right of the green box to copy your shareable link to the clipboard. Return to red ink to share your link with your editor!',
 			imgUrl:
 				'https://res.cloudinary.com/scimgcloud/image/upload/v1606077466/red-ink/gDocsGetShareLink_seuqz7.png',
+			classNameEnabled: true,
+			hrClass: false,
 		},
 	]);
 
@@ -42,6 +50,7 @@ const SubmissionChecklist = () => {
 		const newBoxes = [...checked];
 		console.log(Array.isArray(checked));
 		newBoxes[boxIndex].checked = !newBoxes[boxIndex].checked;
+		newBoxes[boxIndex].classNameEnabled = !newBoxes[boxIndex].classNameEnabled;
 		setChecked(newBoxes);
 	};
 
@@ -51,25 +60,22 @@ const SubmissionChecklist = () => {
 	const emptyBox = 'fas fa-square';
 	const checkedBox = 'fas fa-check-square';
 
-	let enabledRoute;
-	let enabledClass;
-	let enabledText;
-	let enabledIcon;
-
-	if (checked.filter((item) => item.checked === true).length === 4) {
-		enabledRoute = '/submissionform';
-		enabledText = 'Proceed to submit writing';
-		enabledClass = 'enabled';
-		enabledIcon = <i className='fas fa-angle-double-right'></i>;
-	} else {
-		enabledRoute = '/submissionchecklist';
-		enabledText = 'Complete all steps to proceed';
-		enabledClass = 'disabled';
-		enabledIcon = '';
-	}
+	const proceedButton =
+		checked.filter((item) => item.checked === true).length === 4 ? (
+			<button className={`button is-primary proceed-button enabled`}>
+				<Link to='/submissionform'>
+					Proceed to submit writing
+					<i className='fas fa-angle-double-right'></i>
+				</Link>
+			</button>
+		) : (
+			<button className={`button is-primary proceed-button disabled`}>
+				<Link to='/submissionchecklist'>Complete all steps to proceed</Link>
+			</button>
+		);
 
 	const instructions = checked.map((item) => (
-		<>
+		<div className='instructions-container'>
 			<div className='instructions-text' key={item.number}>
 				<div
 					box-number={item.number.toString()}
@@ -77,25 +83,27 @@ const SubmissionChecklist = () => {
 					className={item.checked ? completeClass : incompleteClass}>
 					<i className={item.checked ? checkedBox : emptyBox}></i>
 				</div>
-				<p>{item.text}</p>
+				<p className={item.classNameEnabled ? 'enabled' : 'disabled'}>
+					{item.text}
+				</p>
 			</div>
 			<div className='instructions-img-container'>
-				<img src={item.imgUrl} alt={`img-${item.number}`} />
+				<img
+					src={item.imgUrl}
+					alt={`img-${item.number}`}
+					className={item.classNameEnabled ? 'enabled' : 'disabled'}
+				/>
 			</div>
-		</>
+			<hr className={item.hrClass ? 'hr-visible' : 'hr-invisible'}></hr>
+		</div>
 	));
 
 	return (
-		<>
+		<div className='submissions-checklist-page'>
 			<p id='instructions-intro'>Instructions intro</p>
 			<div className='submission-checklist-container'>{instructions}</div>
-			<button className={`button is-primary proceed-button ${enabledClass}`}>
-				<Link to={enabledRoute}>
-					{enabledText}
-					{enabledIcon}
-				</Link>
-			</button>
-		</>
+			{proceedButton}
+		</div>
 	);
 };
 
