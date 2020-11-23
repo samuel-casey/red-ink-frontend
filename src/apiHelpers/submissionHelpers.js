f9c06c64bfeb4c3af3107178f3ece77b2a0e40fc
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-23 13:43:52 -0500

    added firstName lastName / first_name last_name to POST request

--- src/apiHelpers/submissionHelpers.js
+++ src/apiHelpers/submissionHelpers.js
@@ -8,12 +8,14 @@ export const createNewSubmission = async (submissionData, url) => {
 			created_at: createdAt,
 			editor_id: submissionData.editorUid,
 			edits_complete: submissionData.editsComplete,
-			editor_email: submissionData.editor_email,
+			editor_email: submissionData.editorEmail,
 			title: submissionData.title,
 			notes: submissionData.notes,
 			url: submissionData.link,
 			writer_id: submissionData.writerUid,
 			writer_email: submissionData.writerEmail,
+			first_name: submissionData.editorFirstName,
+			last_name: submissionData.editorLastName,
 		};
 		const res = await axios.post(url + '/submissions', newSubmissionDocument);
 		console.log(res.data.data);
