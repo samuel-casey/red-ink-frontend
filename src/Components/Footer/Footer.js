704b30ecaab674187d2c6c2e89961cda4cd42893
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-24 14:04:48 -0500

    finished Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Footer.scss';
+import { GlobalCtx } from '../../App';
 
 const Footer = () => {
+	const { gState } = useContext(GlobalCtx);
+	const { errorDropdown } = gState;
+
 	return (
-		<div id='footer-container'>
-			<a>Contact</a>
-			<a>Link 2</a>
-			<a>Link 3</a>
-		</div>
+		<>
+			<p className='error-message'>
+				{errorDropdown ? (
+					<span>
+						<span>
+							Woops! Looks like there was an error. Check the top of the screen
+							for more detail
+						</span>{' '}
+						<i className='fas fa-sort-up'></i>
+					</span>
+				) : null}
+			</p>
+			<div id='footer-container'>
+				<a
+					href='mailto:red.ink.user.help@gmail.com'
+					target='_blank'
+					rel='noopener noreferrer'
+					className='tooltip'>
+					Contact Us or Report a Bug
+					<span className='tooltip-text'>red.ink.user.help@gmail.com</span>
+				</a>
+				<a href='https://samcasey.info' target='blank'>
+					About the developer
+				</a>
+			</div>
+		</>
 	);
 };
 
