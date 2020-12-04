928a13b1266803ea588b096c4ef933b0b0e04830
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-12-04 17:09:02 -0500

    added basic error checking via storage rules and an alert message to apiHelpers/storage file to alert user to upload only image files'

--- src/apiHelpers/storage.js
+++ src/apiHelpers/storage.js
@@ -2,13 +2,20 @@ import { fbase } from '../index';
 
 export const createFirebaseStorageURL = async (newFile) => {
 	// create a new item in Firebase Storage for the file
-	const storageRef = fbase.storage().ref();
-	const newFileRef = storageRef.child(newFile.name);
-	await newFileRef.put(newFile);
+	try {
+		const storageRef = fbase.storage().ref();
+		const newFileRef = storageRef.child(newFile.name);
+		await newFileRef.put(newFile);
 
-	// get the URL for the new Firebase Storage item
-	const newFileUrl = await newFileRef.getDownloadURL();
+		// get the URL for the new Firebase Storage item
+		const newFileUrl = await newFileRef.getDownloadURL();
 
-	// return new file URL for use in set state hook
-	return newFileUrl;
+		// return new file URL for use in set state hook
+		return newFileUrl;
+	} catch (error) {
+		// this alert message intentionally has weird formatting so that it looks cleaner in the browser
+		alert(`Error: ${error}.
+        
+PLEASE ENSURE THAT YOU ARE ATTEMPTING TO UPLOAD AN IMAGE WITH A FILE TYPE OF .png, .jpg, or .jpeg`);
+	}
 };
